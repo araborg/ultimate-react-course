7ce208fa755737193ef635889a10d8c4343ae891
araborg <arababs2015@gmail.com>
2025-10-23 22:00:33 +0100

    added .gitignore to d folders

--- 05-travel-list/final/src/components/App.js
+++ 05-travel-list/final/src/components/App.js
@@ -5,43 +5,43 @@ import PackingList from "./PackingList";
 import Stats from "./Stats";
 
 export default function App() {
-  const [items, setItems] = useState([]);
+	const [items, setItems] = useState([]);
 
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]);
-  }
+	function handleAddItems(item) {
+		setItems((items) => [...items, item]);
+	}
 
-  function handleDeleteItem(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
-  }
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
 
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
+	function handleToggleItem(id) {
+		setItems((items) =>
+			items.map((item) =>
+				item.id === id ? { ...item, packed: !item.packed } : item
+			)
+		);
+	}
 
-  function handleClearList() {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete all items?"
-    );
+	function handleClearList() {
+		const confirmed = window.confirm(
+			"Are you sure you want to delete all items?"
+		);
 
-    if (confirmed) setItems([]);
-  }
+		if (confirmed) setItems([]);
+	}
 
-  return (
-    <div className="app">
-      <Logo />
-      <Form onAddItems={handleAddItems} />
-      <PackingList
-        items={items}
-        onDeleteItem={handleDeleteItem}
-        onToggleItem={handleToggleItem}
-        onClearList={handleClearList}
-      />
-      <Stats items={items} />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Logo />
+			<Form onAddItems={handleAddItems} />
+			<PackingList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+				onToggleItem={handleToggleItem}
+				onClearList={handleClearList}
+			/>
+			<Stats items={items} />
+		</div>
+	);
 }
